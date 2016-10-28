948aedbbf950bb83b1489ecd2e77f0a00c459660
ZOORoman <prof-roman@bk.ru>
2016-10-28 18:13:09 +0300

    Update iterators.py

--- librip/iterators.py
+++ librip/iterators.py
@@ -7,11 +7,21 @@ class Unique(object):
         # Например: ignore_case = True, Aбв и АБВ разные строки
         #           ignore_case = False, Aбв и АБВ одинаковые строки, одна из них удалится
         # По-умолчанию ignore_case = False
-        pass
+        self.ind = 0
+        self.unique_items = []
+        self.items=items
+        self.items = iter(self.items)
+        self.ignore_case = 'ignore_case' in kwargs.keys() and kwargs['ignore_case'] is True
 
     def __next__(self):
-        # Нужно реализовать __next__    
-        pass
+        while True:
+            a = next(self.items)
+            b = str(a)
+            if self.ignore_case is True:
+                b = b.lower()
+            if b not in self.unique_items:
+                self.unique_items.append(b)
+                return a
 
     def __iter__(self):
         return self
