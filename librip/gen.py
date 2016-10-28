c6c0282ee1d6afd1edab9af047563c1c1283a0a6
ZOORoman <prof-roman@bk.ru>
2016-10-28 18:12:08 +0300

    Update gen.py

--- librip/gen.py
+++ librip/gen.py
@@ -12,7 +12,20 @@ import random
 
 def field(items, *args):
     assert len(args) > 0
-    # Необходимо реализовать генератор 
+    if (len(args) == 1):
+        for element in items:
+            if element.get(args[0]):
+                yield element.get(args[0])
+
+
+    else:
+        for element in items:
+            dict = {}
+            for key in args:
+                if (key in element):
+                    dict[key] = element[key]
+            if len(dict)>0:
+                yield dict
 
 
 # Генератор списка случайных чисел
@@ -20,5 +33,6 @@ def field(items, *args):
 # gen_random(1, 3, 5) должен выдать примерно 2, 2, 3, 2, 1
 # Hint: реализация занимает 2 строки
 def gen_random(begin, end, num_count):
-    pass
+    for i in range(num_count):
+        yield random.randint(begin, end)
     # Необходимо реализовать генератор
